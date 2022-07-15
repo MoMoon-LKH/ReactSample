96dc64c3ff94ac72d22631fc01550fce08c17428
MoMoon-LKH <rlgh28@naver.com>
2022-07-15 15:13:34 +0900

    켐포넌트, prop, 이벤트

--- sample/src/App.js
+++ sample/src/App.js
@@ -1,24 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+function Header(props){ // 함수 -> 컴포넌트, 매개변수 -> Prop 라고 불림
+  return <header>
+        <h1><a href='/' onClick={(event)=>{
+          event.preventDefault();
+          props.onchangetMode();
+        }}>{props.title}</a></h1>
+      </header>
+}
+
+function Nav(props){
+  const lis = [];
+
+  for(let i = 0; i < props.topics.length; i++){
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a id={t.id} href={'/read' + t.id} onClick={(event)=>{
+      event.preventDefault();
+      props.onchangetMode(event.target.id);
+    }
+    }>{t.title}</a></li>);
+  }
+
+  return       <nav>
+  <ol>
+    {lis}
+  </ol>
+</nav>
+
+}
+
+
+function Article(props){
+  return <article>
+        <h2>{props.title}</h2>
+        {props.body}
+      </article>
+}
+
 function App() {
+  const topics = [
+    {id:1, title:'html', body:'html is ....'},
+    {id:2, title:'css', body:'css is ....'},
+    {id:3, title:'js', body:'javascript is ....'},
+  ];
+
   return (
     <div>
-      <header>
-        <h1><a href='/'>WEB</a></h1>
-      </header>
-      <nav>
-        <ol>
-          <li><a href='/read/1'>html</a></li>
-          <li><a href='/read/2'>css</a></li>
-          <li><a href='/read/3'>js</a></li>
-        </ol>
-      </nav>
-    
-      <article>
-        <h2>Welcome</h2>
-        Hello, WEB
-      </article>
+      <Header title='REACT' onchangetMode={function(){
+        alert("Header");
+      }}></Header>
+      <Nav topics={topics} onchangetMode={(id) =>{
+        alert(id);}
+      }></Nav>    
+      <Article title='Welcome' body='Hello, WEB'></Article>
+      <Article title='Hi' body='Hello, React'></Article>
     </div>
   );
 }
